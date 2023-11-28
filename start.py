7d91f019261278c9f3ead409e3f92d9d898760d3
KREAMpresLIQ <clarkkent8637@gmail.com>
2023-11-28 19:32:18 +0100

    START

---
+++ start.py
@@ -0,0 +1,30 @@
+# Write your code below this line
+
+print('Day 1 - Python Print Function')
+print('The function is declared like this: ')
+print("print('what to print')")
+
+
+# String manipulation
+print("Hello world!\nHello World!\nHello World!")
+print("Hello" + " " + "Norbert")
+
+# Debugging
+# Fix the code below
+
+# print(Day 1 - String Manipulation")
+# print("String Concatenation is done with the "+" sign.")
+#     print('e.g. print("Hello " + "world")')
+# print(("New lines can be created with a backslash and n."))
+
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+
+# Input Function
+#Thonny#
+
+print("Hello" + " " + input("What is your name?") + "!")
