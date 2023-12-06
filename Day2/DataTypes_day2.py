30f930324f62110a3cb1d77d771ce7c6b8ca727d
KREAMpresLIQ <clarkkent8637@gmail.com>
2023-12-06 18:20:39 +0100

    DataTypes

--- Day2/DataTypes_day2.py
+++ Day2/DataTypes_day2.py
@@ -105,6 +105,7 @@ print(f"You have {age_days} days, {age_weeks} weeks, and {age_months} months lef
 # If the bill was $150.00, split between 5 people, with 12% tip.
 
 # Each person should pay (150.00 / 5) * 1.12 = 33.6
+
 # Format the result to 2 decimal places = 33.60
 
 # Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
