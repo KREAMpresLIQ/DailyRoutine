495b7af69363fad7847ec6a0fc5e8f00dc8ba8e1
KREAMpresLIQ <clarkkent8637@gmail.com>
2023-12-06 22:51:42 +0100

    ControlFlow_LeapYear

--- Day3/ControlFlow_LogicalOperators.py
+++ Day3/ControlFlow_LogicalOperators.py
@@ -47,3 +47,46 @@ else:
 # print(module_even_2)
 # module_even_3 = 8 % 2
 # print(module_even_3)
+
+###### BMI 2.0
+
+#  Don't change the code below
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+#  Don't change the code above
+
+# Write your code below this line
+
+BMI = (int(weight) / float(height) ** 2)
+BMI_as_int = round(int(BMI))
+print(BMI_as_int)
+
+if BMI_as_int < 18.5:
+    print(f"Your BMI is {BMI_as_int}, you are underweight")
+elif 18.5 <= BMI_as_int < 25:
+    print(f"Your BMI is {BMI_as_int}, you have a normal weight")
+elif 25 <= BMI_as_int < 30:
+    print(f"Your BMI is {BMI_as_int}, you are slightly overweight")
+elif 30 <= BMI_as_int < 35:
+    print(f"Your BMI is {BMI_as_int}, you are obese")
+else:
+    print(f"Your BMI is {BMI_as_int}, you are clinically obese")
+
+###### LEAP YEAR
+
+#  Don't change the code below
+year = int(input("Which year do you want to check? "))
+#  Don't change the code above
+
+#Write your code below this line
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap Year.")
+        else:
+            print("Not Leap Year.")
+    else:
+        print("Leap Year.")
+else:
+    print("Not Leap Year.")
