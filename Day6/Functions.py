0ad851ad5fa368df28527622d8cf44fda0eafb7f
KREAMpresLIQ <clarkkent8637@gmail.com>
2024-01-10 22:17:56 +0100

    Defining and Calling Functions

---
+++ Day6/Functions.py
@@ -0,0 +1,68 @@
+## https://docs.python.org/3/library/functions.html
+
+# print("Hello")
+# num_char = len("Hello")
+# print(num_char)
+
+#####################
+# Defining Functions
+# def my_function():
+#     Do this
+#     Then do this
+#     Finally do this
+
+####################
+# Calling the function
+
+# my_function()
+
+######################
+# Reeborg
+
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json
+
+#### Codes
+# def turn_around():
+#     turn_left()
+#     turn_left()
+#
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+# def move_three_step():
+#     move()
+#     move()
+#     move()
+#
+# def build_wall_random():
+#     move_three_step()
+#     turn_left()
+#     move_three_step()
+#     move_three_step()
+#     turn_right()
+#     move_three_step()
+#     turn_left()
+#     move_three_step()
+#     turn_right()
+#     move_three_step()
+#     turn_around()
+#     move_three_step()
+#     move_three_step()
+#     turn_left()
+#     move()
+#     build_wall()
+#
+# def DrawSquare():
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#
+# DrawSquare()
