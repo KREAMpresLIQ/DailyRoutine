6a3ff79420848aea26367857c4b384be5d6a0719
KREAMpresLIQ <clarkkent8637@gmail.com>
2024-01-17 00:29:10 +0100

    Defining and Calling Functions

--- Day6/Functions.py
+++ Day6/Functions.py
@@ -20,48 +20,35 @@
 # Reeborg
 # https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json
 
-#### Codes
-# def turn_around():
-#     turn_left()
-#     turn_left()
-#
-# def turn_right():
-#     turn_left()
-#     turn_left()
-#     turn_left()
-#
-# def move_three_step():
-#     move()
-#     move()
-#     move()
-#
-# def build_wall_random():
-#     move_three_step()
-#     turn_left()
-#     move_three_step()
-#     move_three_step()
-#     turn_right()
-#     move_three_step()
-#     turn_left()
-#     move_three_step()
-#     turn_right()
-#     move_three_step()
-#     turn_around()
-#     move_three_step()
-#     move_three_step()
-#     turn_left()
-#     move()
-#     build_wall()
-#
-# def DrawSquare():
-#     turn_left()
-#     move()
-#     turn_right()
-#     move()
-#     turn_right()
-#     move()
-#     turn_right()
-#     move()
-#     turn_right()
-#
-# DrawSquare()
+
+######################
+# Indentation
+
+sky = "clear"
+def my_function():
+    if sky == "clear":
+        print("Blue")
+    elif sky == "cloudy":
+        print("Grey")
+    print("Hello")
+print("World")
+
+my_function()
+
+######################
+# While loops
+
+######## For
+for item in list_of_items:
+    # Do something to each item
+
+for number in range(a,b):
+    print(number)
+
+######## While
+
+while something_is_ture:
+    # Do something repeatedly
+
+
+
