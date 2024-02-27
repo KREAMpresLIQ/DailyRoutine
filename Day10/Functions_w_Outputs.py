0c4fadcb9f53027a17b968bd07cb2f9dc4506d32
KREAMpresLIQ <clarkkent8637@gmail.com>
2024-02-28 00:24:04 +0100

    Functions with Outputs

---
+++ Day10/Functions_w_Outputs.py
@@ -0,0 +1,20 @@
+# Functions with Outputs
+
+def my_function():
+    result = 3 * 2
+    return result
+
+
+def format_name(f_name, l_name):
+    formated_f_name = (f_name.title())
+    formated_l_name = (l_name.title())
+
+    # print(f"{formated_f_name} {formated_l_name}")
+    return f"{formated_f_name} {formated_l_name}"
+
+
+# format_name("CSAJKAS","norbert")
+
+print(format_name("CSAJKAS", "norbert"))
+
+output = len("Norbert")
